21b733de875c3754288be4368e3f0aec18a37fae
iamjoee <joeadvocates123@gmail.com>
2023-06-12 18:04:10 +0300

    morefrontend

--- MySQL/posttomysql/process.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE students (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(50) NOT NULL,
-  email VARCHAR(50) NOT NULL,
-  phone VARCHAR(20) NOT NULL
-  -- Add more columns as needed
-);
